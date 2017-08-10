5b689639e70ed972c65c4c820664fa6205364b1d
paul hoover <ph@selectablemedia.com>
2017-08-10 09:00:36 -0400

    first commit after initial add. todo app wip

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <h1>todo list app</h1>
+    <todo-list v-bind:todos="todos"></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'app',
+  components: {
+    TodoList
+  },
+  data() {
+    return {
+      todos: [
+        {
+          title: 'todo a',
+          description: 'the first todo',
+          project: 'acme',
+          done: false
+        },
+        {
+          title: 'todo b',
+          description: 'the second todo',
+          project: 'aaa',
+          done: true
+        },
+        {
+          title: 'todo c',
+          description: 'the third todo',
+          project: 'aaa',
+          done: false
+        },
+        {
+          title: 'todo d',
+          description: 'the forth todo',
+          project: 'acme',
+          done: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='list-holder'>
+      <todo  v-for="todo in todos" v-bind:todo="todo"></todo>
+  </div>
+</template>
+<script type='text/javascript'>
+import Todo from './Todo';
+
+export default {
+  props: ['todos'],
+  components: {
+    Todo
+  }
+}
+</script>
+<style>
+.list-holder {
+  text-align: left;
+}
+
+.list-holder ul {
+  list-style: none;
+}
+</style>
